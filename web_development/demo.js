c1190227f24548a2f10cfa0eacb8c0017cf7c456
iii <134790985+awiana@users.noreply.github.com>
2024-07-01 23:59:24 +0800

    Create demo.js

---
+++ web_development/demo.js
@@ -0,0 +1,26 @@
+function summarize() {
+    if (!document.getElementById("myform").reportValidity()) {
+        return false;
+    }
+
+    var cname = document.getElementById("cname").value;
+    var cell = document.getElementById("cell").value;
+
+    document.getElementById("cinfo").innerHTML = "Customer Name: "+cname+"<br>Cell Number: "+cell;
+
+    if (document.getElementById("size_s").checked) {
+        document.getElementById("cost").innerHTML = "Cost: &#8369; 80.00";
+        document.getElementById("addr").disabled = true;
+        document.getElementById("addr").required = false;
+    }
+    else if (document.getElementById("size_m").checked) {
+        document.getElementById("cost").innerHTML = "Cost: &#8369; 100.00";
+        document.getElementById("addr").disabled = false;
+        document.getElementById("addr").required = true;
+    }
+    else if (document.getElementById("size_l").checked) {
+        document.getElementById("cost").innerHTML = "Cost: &#8369; 120.00";
+        document.getElementById("addr").disabled = false;
+        document.getElementById("addr").required = true;
+    }
+}
